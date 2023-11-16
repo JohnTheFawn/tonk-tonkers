440121b5a7b7a2e368957347905a54329bcc6881
John Mitchell <johnthefawn91@gmail.com>
2023-11-16 18:03:20 -0500

    added links to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Tonk Tonkers <code className={styles.code}>src/app/page.tsx</code>
+          <Link href="/">Tonk Tonkers <code className={styles.code}>src/app/page.tsx</code></Link>
+        </p>
+      </div>
+      <div className={styles.description}>
+        <p>
+          <Link href="/fflogs">FF Logs <code className={styles.code}>src/app/fflogs/page.tsx</code></Link> 
         </p>
       </div>
     </main>
