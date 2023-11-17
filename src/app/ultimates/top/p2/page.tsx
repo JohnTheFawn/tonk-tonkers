c325ab63bb88371c16184e1110839f89034bbe05
John Mitchell <johnthefawn91@gmail.com>
2023-11-17 03:10:45 -0500

    added some damage numbers

--- src/app/ultimates/top/p2/page.tsx
+++ src/app/ultimates/top/p2/page.tsx
@@ -24,10 +24,10 @@ export default function Home() {
                 <p>- Female&#39;s auto attacks are magic damage.</p>
 
                 <hr/>
-                <h3>Solar Ray Tankbusters (165,000 - 190,000)</h3>
+                <h3>Solar Ray Tankbusters (165,000 - 190,000) and Auto Attack x3 (108,000)</h3>
                 <p>- Rampart needs to be used when Omega jumps away in Phase 1.</p>
                 <p>- Tanks use all your mits except your 30% (Sentinel/Vengeance/Shadow Wall/Nebula).</p>
-                <p>- The following 3 autos do about 38,000 each (108,000 total) so try to have some mit/healing to handle this as well (second sheltron, equilibrium, second oblation, and aurora). You will still most likely need help from healers</p>
+                <p>- The following 3 autos do about 38,000 each (108,000 total, 3s between autos) so try to have some mit/healing to handle this as well (second sheltron, equilibrium, second oblation, and aurora). You will still most likely need help from healers</p>
                 <p>- Tank 1 can use Reprisal on this. It will only work on their target due to the Packet Filter debuff.</p>
 
                 <hr/>
@@ -54,6 +54,7 @@ export default function Home() {
                 <p>- While waiting for the clones to resolve, find the eye if you haven&#39;t. This is your new North.</p>
                 <p>- Mid Glitch: both sides line up in the same BPOG (From North to South: Blue, Pink, Orange, Green) order on each side. Line up with the outside of the markers (or two tick marks if looking at the edge of the arena). If you stand too close to your partner, both of you get a vulnerability stack and die.</p>
                 <p>- Remote Glitch: the right side will adjust their line up to GPOB (From North to South: Green, Pink, Orange, Blue. Green and Blue Swap). All the way to the wall. Square and Circle should be 3-4 tick marks away from the middle so they don&#39;t overlap. X and Triangle still stand infront of the second tick mark.</p>
+                <p>- Spread Damage: 29,000-33,000</p>
                 <div className={styles.description}>
                     <div className={styles.textAlignCenter}>
                         Mid Glitch (BPOG-BPOG)
@@ -80,6 +81,7 @@ export default function Home() {
                 <p>- If you were the North-most stack and had to flex, this is where that happens, make sure you get knocked to the appropriate side.</p>
                 <p>- Mid Glitch: Just outside the marker you get knocked to. If you are looking at rings on the ground that would be the inner edge of the 2nd ring. Careful not to get clipped by the males.</p>
                 <p>- Remote Glitch: To the wall.</p>
+                <p>- Stack Damage: 50,000 - 60,000</p>
                 <div className={styles.description}>
                     <div className={styles.textAlignCenter}>
                         Mid Glitch - Behind the marker
