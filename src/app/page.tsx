01405bc76d3d1482ab9d1f1922d2d4d8c1995080
John Mitchell <johnthefawn91@gmail.com>
2023-11-17 19:01:01 -0500

    cleaning up some more css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,23 @@
-import styles from './page.module.css'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <Link href="/">
-          <p>
-            Tonk Tonkers | <code className={styles.code}>src/app/page.tsx</code>
-          </p>
-        </Link>
-      </div>
-      <div className={styles.description}>
-        <Link href="/fflogs">
-          <p>
-            FF Logs | <code className={styles.code}>src/app/fflogs/page.tsx</code> 
-          </p>
-        </Link>
-      </div>
-      <div className={styles.description}>
-        <Link href="/ultimates">
-          <p>
-            Ultimate Guides | <code className={styles.code}>src/app/ultimates/page.tsx</code> 
-          </p>
-        </Link>
-      </div>
+    <main className={"cardWrapper"}>
+      <Link className={"cardLink"} href="/">
+        <p>
+          Tonk Tonkers | <code className={"code"}>src/app/page.tsx</code>
+        </p>
+      </Link>
+      <Link className={"cardLink"} href="/fflogs">
+        <p>
+          FF Logs | <code className={"code"}>src/app/fflogs/page.tsx</code> 
+        </p>
+      </Link>
+      <Link className={"cardLink"} href="/ultimates">
+        <p>
+          Ultimate Guides | <code className={"code"}>src/app/ultimates/page.tsx</code> 
+        </p>
+      </Link>
     </main>
   )
 }
